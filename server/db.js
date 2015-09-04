d5e455d41585d503626383dbdc0654ca1414d71a
Rui Ramos <rui@ometria.com>
2015-09-04 12:10:37 +0100

    db

--- server/db.js
+++ server/db.js
@@ -7,13 +7,22 @@ var messages = {};
 var MAX = 50;
 
 function save(msg, room){
-  messages[room] = messages[room] || [];
-  messages[room].push(msg);
-  if(messages[room].length > MAX){
-    messages[room].splice(0, messages[room].length - MAX);
-  }
+  if(messages[room]){
+    messages[room].push(msg);
+    if(messages[room].length > MAX){
+      messages[room].splice(0, messages[room].length - MAX);
+    }
 
-  client.set("vchat:"+room, JSON.stringify(messages[room]), 'EX', (60 * 60 * 24 * 1), redis.print);
+    client.set("vchat:"+room, JSON.stringify(messages[room]), 'EX', (60 * 60 * 24 * 1), redis.print);
+
+  } else {
+    // trying to get again just to be sure
+    client.get("vchat:"+room, function(err, res){
+      messages[room] = res ? JSON.parse(res) : [];
+      // run again with messages set
+      save(msg, room, true);
+    });
+  }
 
 }
 
@@ -33,6 +42,7 @@ function getAll(room, fn){
     });
   }
 }
+
 
 module.exports = {
   save: save,
