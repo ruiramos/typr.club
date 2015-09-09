293e8d4c9d3a6ce9e59302e352788a07c265e74b
Rui Ramos <rui@ometria.com>
2015-09-09 22:24:36 +0100

    stuff

--- server/io.js
+++ server/io.js
@@ -7,6 +7,7 @@ var WebSocketServer = require('ws').Server,
 
 function connect(port){
   wss = new WebSocketServer({ port: port });
+  var self = this;
 
   wss.on('connection', function connection(ws) {
     var room = ws.upgradeReq.url.replace('/', '');
@@ -14,24 +15,25 @@ function connect(port){
     clients[room].push(ws);
 
     db.getWithOffset(room, 0, function(res){
-      ws.send(JSON.stringify(res));
+      ws.send(JSON.stringify({type: 'message:load', data: res}));
     })
+
+    self.broadcast({type: 'presence', data: {clients: clients[room].length}}, room);
 
     ws.on('message', function(res){
       var data = JSON.parse(res);
       switch(data.type){
         case 'request:load':
           db.getWithOffset(room, data.offset || 0, function(videos){
-            ws.send(JSON.stringify(videos));
+            ws.send(JSON.stringify({type: 'message:new', data: videos}));
           })
           break;
       }
-
-
     })
 
     ws.on('close', function(){
       clients[room].splice(clients[room].indexOf(ws), 1);
+      self.broadcast({type: 'presence', data: {clients: clients[room].length}}, room);
     })
   });
 
