e8fdfb168512962de05e0dabb2be35a8ef11506d
Rui Ramos <rui@ometria.com>
2015-12-07 16:13:39 +0100

    asd

--- server/handlers.js
+++ server/handlers.js
@@ -132,8 +132,8 @@ function thumb(response, _, request){
 
   phantom.create({parameters: {'ignore-ssl-errors': 'yes', 'load-images': 'yes', 'local-to-remote-url-access': 'yes'}}, function(ph) {
     return ph.createPage(function(page) {
-      page.setViewportSize(1440, 800);
-      page.set('clipRect', {top: 0, left: 0, width: 1440, height: 800});
+      page.setViewportSize(1440, 900);
+      page.set('clipRect', {top: 0, left: 0, width: 1440, height: 900});
 
       return page.open("http://localhost:8000/"+id+"?render=true", function(status) {
         setTimeout(function(){
